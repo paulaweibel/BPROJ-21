19ba0800028203d7a567cc721ab695df8944264e
Paula Weibel <paula.weibel@stud.hslu.ch>
2021-05-07 01:01:27 +0200

    dialogue

--- code experiments/scene/js/main.js
+++ code experiments/scene/js/main.js
@@ -20,15 +20,57 @@ window.addEventListener('mousemove', (evt) => {
 var phrase1 = document.querySelector("#phrase1");
 var phrase2 = document.querySelector("#phrase2");
 var phrase3 = document.querySelector("#phrase3");
+var phrase4 = document.querySelector("#phrase4");
 
 var backgroundcolor = document.querySelector("#background-color");
+var backSVG = document.querySelector(".stress-path");
 
 //SET STORY TEXTS INVISIBLE
 phrase1.style.display = "none";
 phrase2.style.display = "none";
 phrase3.style.display = "none";
+phrase4.style.display = "none";
+
+//////////////////////////////////////////////
+
+///Story Interactions
 
 
+//FAKE INTERACTIONS
+var trigger2 = 0;
+
+phrase2.addEventListener('click', function() {
+  trigger2 = trigger2 + 1;
+  console.log("trigger count: " + trigger2)
+  if (trigger2 == 1) {
+    phrase2.style.transform = "translate(0,-100px)";
+    phrase2.style.opacity = "0.7";
+  }
+  if (trigger2 == 2){
+    phrase2.style.transform = "translate(-100px,-100px)";
+    phrase2.style.opacity = "0.4";
+  }
+  if (trigger2 == 3){
+    phrase2.style.transform = "translate(-100px,-200px)";
+    phrase2.style.opacity = "0.2";
+  }
+  if (trigger2 == 4){
+    phrase2.style.transform = "translate(-200px,-200px)";
+    phrase2.style.opacity = "0";
+  }
+});  
+
+
+
+//// real interaction
+phrase3.addEventListener('click', function() {
+  console.log("clicked");
+  phrase1.style.display = "none";
+  phrase2.style.display = "none";
+  phrase3.style.display = "none";
+  phrase4.style.display = "block";
+  backSVG.style.transform = "scale(1)";
+});  
 
 
 
@@ -56,29 +98,29 @@ function animation(scrollPos) {
 console.log(scrollLocation)
 
 // ANIMATIONS BASED ON SCROLL POSITION
-  if (scrollLocation > 1 && scrollLocation < 600){
+  if (scrollLocation >= 0){
     phrase1.style.display = "block";
   } else{
     phrase1.style.display = "none";
   }
 
-  if (scrollLocation > 301 && scrollLocation < 1024){
+  if (scrollLocation > 501){
     phrase2.style.display = "block";
   } else{
     phrase2.style.display = "none";
   }
 
-  if (scrollLocation > 601 && scrollLocation < 1024){
+  if (scrollLocation > 501){
     phrase3.style.display = "block";
   } else{
     phrase3.style.display = "none";
   }
 
-  if (scrollLocation > 500){
-    backgroundcolor.style.backgroundColor = "#000";
-  } else{
-    backgroundcolor.style.backgroundColor = "#fff";
-  }
+  // if (scrollLocation > 500){
+  //   backgroundcolor.style.backgroundColor = "#000";
+  // } else{
+  //   backgroundcolor.style.backgroundColor = "#fff";
+  // }
 
 }
 
