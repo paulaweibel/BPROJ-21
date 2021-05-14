a345a014fb8759c593afd7c8f3953ab3a5faae75
Paula Weibel <paula.weibel@stud.hslu.ch>
2021-05-14 17:44:00 +0200

    merge smth

--- bproj/js/main.js
+++ bproj/js/main.js
@@ -111,6 +111,8 @@ phrase4.style.display = "none";
 // phrase7.style.display = "none";
 // phrase8.style.display = "none";
 spotlight.style.display = "none";
+helpYes.style.display = "block";
+helpNo.style.display = "block";
 
 
 
